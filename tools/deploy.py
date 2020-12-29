35a2ff9c7d7b017cc4465ec24c5bbd5f3977c57e
Calvin Ng <calkwng@gmail.com>
2020-12-28 21:37:20 -0800

    Correct argument parser for deploy.py

--- tools/deploy.py
+++ tools/deploy.py
@@ -37,14 +37,14 @@ def main(args):
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description="Build specified Endless ATC airports and copy to the Endless ATC directory.", epilog="https://github.com/AdamJCavanaugh/EndlessATCAirports")
-	parser.add_argument('codes', nargs='*', help="Airport codes to build and deploy. Prefixes can be used. If no code specified, you will be prompted if all airport files are to be processed.")
-	parser.add_argument('-w', '--input-dir', nargs='?', default=os.path.join(os.pardir, 'final'), help='''The directory containing the airport files. 
+	parser.add_argument('codes', nargs='*', help="Airport codes to build and deploy. Prefixes can be used. If no code specified, you will be prompted if all airport files are to be processed.", metavar="code")
+	parser.add_argument('-w', '--input-dir', default=os.path.join(os.pardir, 'final'), help='''The directory containing the airport files. 
 		Subdirectories that are not source directories will also be searched. Defaults to '../final'.''')
-	parser.add_argument('-s', '--source-dir', nargs='?', default='source', help="The name of the folders that will contain source files. Defaults to 'source'.")
-	parser.add_argument('-p', '--pattern', nargs='?', default='{code}*.txt', help="The glob pattern for the file names to build based on the input codes. Defaults to '{code}*.txt'.")
-	parser.add_argument('-o', '--output-path', nargs='?', default=os.pardir, help='''The path to the directory to store the output of the build process relative to the source file. 
+	parser.add_argument('-s', '--source-dir', default='source', help="The name of the folders that will contain source files. Defaults to 'source'.")
+	parser.add_argument('-p', '--pattern', default='{code}*.txt', help="The glob pattern for the file names to build based on the input codes. Defaults to '{code}*.txt'.")
+	parser.add_argument('-o', '--output-path', default=os.pardir, help='''The path to the directory to store the output of the build process relative to the source file. 
 		Defaults to the parent directory relative to the source file.''')
-	parser.add_argument('-d', '--destination-path', nargs='?', help='''The directory to copy the output of the build process to (e.g. Endless ATC locations folder). 
+	parser.add_argument('-d', '--destination-path', help='''The directory to copy the output of the build process to (e.g. Endless ATC locations folder). 
 		Defaults to "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Endless ATC\\locations\\".
 		This default can be overridden by a 'path = ' entry under a [deploy] section in an eatcdev.ini.''')
 	parser.add_argument('-n', '--no-build', action='store_false', dest='build', help='Specify to skip build, and just copy sources to output folder.')
